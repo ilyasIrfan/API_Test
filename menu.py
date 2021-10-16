0db0848ba5bfac024b99558fd8614ca62b2549d3
wpo9nine <wpo9nine@gmail.com>
2021-10-16 11:16:06 +0700

    add user db

--- menu.py
+++ menu.py
@@ -1,16 +1,36 @@
 import uvicorn
 import json
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from typing import Optional
 from pydantic import BaseModel
+
+fake_users_db = {
+    "Ilyas": {
+        "username": "Ilyas",
+        "full_name": "Ilyas Irfan",
+        "email": "ilyas@gmail.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "Irfan",
+        "full_name": "Irfan Syiraaj",
+        "email": "irfan@gmail.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
 
 with open("menu.json","r") as read_file:
 	data = json.load(read_file)
 
 app = FastAPI()
+
+def fake_hash_password(password: str):
+    return "fakehashed" + password
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -20,16 +40,36 @@ class User(BaseModel):
     full_name: Optional[str] = None
     disabled: Optional[bool] = None
 
+class UserInDB(User):
+    hashed_password: str
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
 def fake_decode_token(token):
-    return User(
-        username=token + "fakedecoded", email="wpo9nine@gmail.com", full_name="Ilyas Irfan"
-    )
+    # return User(
+    #     username=token + "fakedecoded", email="wpo9nine@gmail.com", full_name="Ilyas Irfan"
+    # )
+	user = get_user(fake_users_db, token)
+    return user
 
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     user = fake_decode_token(token)
-    return user
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+	return user
 
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
 
 @app.get("/users/me")
 async def read_users_me(current_user: User = Depends(get_current_user)):
